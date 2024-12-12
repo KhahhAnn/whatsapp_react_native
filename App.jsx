b887e91989bebe7d0160bdbcf2e7a431384e3719
khahhan <khanhanbui1010@gmail.com>
2024-12-12 12:36:56 +0700

    up code sigin register

--- App.jsx
+++ App.jsx
@@ -1,22 +1,61 @@
-import { StyleSheet } from "react-native";
-import MyTabs from "./components/Tabs";
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import MyTabs from "./components/Tabs";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const userData = await AsyncStorage.getItem("userStore");
+        setIsLoggedIn(!!userData); // Nếu `userData` tồn tại, nghĩa là người dùng đã đăng nhập
+      } catch (error) {
+        console.error("Error checking login status:", error);
+      } finally {
+        setLoading(false); // Dừng hiển thị trạng thái loading
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <MyTabs />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {isLoggedIn ? (
+          <Stack.Screen name="Main" component={MyTabs} />
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+          </>
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: "20px",
+  loadingContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
